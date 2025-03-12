1d8f505b0a7e768db93a6af1d8d17b143ef8ef30
adferraz <adferraz.bsi@gmail.com>
2025-03-12 17:41:02 -0300

    first commit

---
+++ src/Contador.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int parametroUm, parametroDois;
+
+        System.out.print("Digite o primeiro parâmetro: ");
+        parametroUm = scanner.nextInt();
+
+        System.out.print("Digite o segundo parâmetro: ");
+        parametroDois = scanner.nextInt();
+
+    }
+}
